c947acd654eaddc505c77e287ac09f9b79297e59
Jan Joseph <jan00joseph@gmail.com>
2021-10-17 20:48:19 +0530

    Slider Change
    1. Slider Input added 2. Slider Value indication tag added 3. CSS change for the relevant tags

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
 export default function Slider() {
+    const sliderStyle = {
+        WebkitAppearance: "none",
+        // background: "linear-gradient(145deg, #f5f5ff, #e6e6e6)",
+        boxShadow: "inset 2px 2px 4px #d9d9d9, inset -2px -2px 4px #ffffff"
+    }
+
+    const sliderValueStyle = {
+        textShadow: "-1px 1px 0 #000, 1px 1px 0 #000, 1px -1px 0 #000, -1px -1px 0 #000"
+    }
     return (
         <div className=" flex flex-col items-center mt-6">
             <p className="self-center">Select the Tile Size</p>
-            <input type="range" name="Tile Size" id="slider" max="100" min="4" defaultValue="10" />
-            <p id="range-value">10</p>
+            <input 
+                className="mt-6 rounded-3xl h-4 w-96 bg-gray-100" 
+                style={sliderStyle}
+                type="range" name="Tile Size"
+                 id="slider" 
+                max="100" min="4" 
+                defaultValue="10" />
+            <p 
+                style={sliderValueStyle}
+                className="text-6xl text-yellow-500"
+                id="range-value">
+                    10
+            </p>
         </div>
     )
 }
