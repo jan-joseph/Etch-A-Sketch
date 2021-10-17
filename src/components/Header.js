2e0af5bb5d973a0860365e66522f433024aeb57d
Jan Joseph <jan00joseph@gmail.com>
2021-10-17 20:45:42 +0530

    CSS Change
    Title text font change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return(
         <header className="App-header">
-            <h1 className="title text-6xl">Sprite-Sketchpad</h1>
+            <h1 className="title text-4xl mt-4 font-bold">Sprite-Sketchpad</h1>
         </header>
     )
 }
