70b3d1265bd99539736d44188729930cc063a2f8
Jan Joseph <jan00joseph@gmail.com>
2021-10-21 21:26:15 +0530

    CSS Change
    Slider and Grid Component CSS updated

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,36 +1,37 @@
 import React from 'react'
 
 export default function Grid() {
-    const gridContainerStyle = {
-        width: '70vh',
-        height: '70vh'
+
+    const Styles = {
+        gridContainerStyle : {
+            width: '70vh',
+            height: '70vh'
+        },
+        gridStyle : {
+            boxShadow: '3px 3px 6px #d9d9d9, -3px -3px 6px #ffffff'
+        },
+        clearBtnStyle : {
+            background: 'linear-gradient(145deg, #ececec, #f1f1f1)',
+            boxShadow:  '4px 4px 4px #d8d8d8, -4px -4px 4px #ffffff',  
+        }
     }
 
-    const gridStyle = {
-        boxShadow: '3px 3px 6px #d9d9d9, -3px -3px 6px #ffffff'
-    }
 
-    const clearBtnStyle = {
-        // background: 'linear-gradient(145deg, #ececec, #f1f1f1)',
-        boxShadow:  '4px 4px 4px #d8d8d8, -4px -4px 4px #ffffff',
-        '&:hover': {
-            textDecoration: 'underline',
-       },
-    }
+
     return (
         <div 
-            style={gridContainerStyle}
+            style={Styles.gridContainerStyle}
             className="flex flex-col items-center justify-center"
         >
             <div id="sketchpad"
                 className = 'grid mt-4 h-full w-full'
-                style={gridStyle}
+                style={Styles.gridStyle}
             >
             </div>
             <button 
+                style = {Styles.clearBtnStyle}
                 id = "clearBtn"
-                style = {clearBtnStyle}
-                className = "mt-4 h-12 w-28 border-none rounded-xl bg-red-700 text-2xl text-white "
+                className = "mt-4 h-12 w-28 border-none rounded-xl text-2xl text-red-700 "
             >
                     Clear
             </button>
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,27 +1,28 @@
 import React from 'react'
 
-export default function Slider() {
-    const sliderStyle = {
-        WebkitAppearance: "none",
-        // background: "linear-gradient(145deg, #f5f5ff, #e6e6e6)",
-        boxShadow: "inset 2px 2px 4px #d9d9d9, inset -2px -2px 4px #ffffff"
-    }
-
-    const sliderValueStyle = {
-        textShadow: "-1px 1px 0 #000, 1px 1px 0 #000, 1px -1px 0 #000, -1px -1px 0 #000"
+const Slider = () => {
+    const Styles = {
+        sliderStyle:{
+            WebkitAppearance: "none",
+            // background: "linear-gradient(145deg, #f5f5ff, #e6e6e6)",
+            boxShadow: "inset 2px 2px 4px #d9d9d9, inset -2px -2px 4px #ffffff"
+        },
+        sliderValueStyle : {
+           textShadow: "-1px 1px 0 #000, 1px 1px 0 #000, 1px -1px 0 #000, -1px -1px 0 #000"
+        },
     }
     return (
         <div className=" flex flex-col items-center mt-6">
             <p className="self-center">Select the Tile Size</p>
             <input 
                 className="mt-6 rounded-3xl h-4 w-96 bg-gray-100" 
-                style={sliderStyle}
+                style={Styles.sliderStyle}
                 type="range" name="Tile Size"
-                 id="slider" 
+                id="slider" 
                 max="100" min="4" 
                 defaultValue="10" />
             <p 
-                style={sliderValueStyle}
+                style={Styles.sliderValueStyle}
                 className="text-6xl text-yellow-500"
                 id="range-value">
                     10
@@ -29,3 +30,6 @@ export default function Slider() {
         </div>
     )
 }
+
+
+export default Slider;
